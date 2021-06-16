7ec29e1bd91994c880ebcc677858412074d4fa7e
pCpptgzoh <ztt68692915@163.com>
2021-06-16 18:42:33 +0800

    adRedis 解决广告缓存  可以实现清除缓存

--- pinyougou-content-service-1/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou-content-service-1/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -1,6 +1,9 @@
 package com.pinyougou.content.service.impl;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -14,6 +17,7 @@ import entity.PageResult;
 
 /**
  * 服务实现层
+ * 
  * @author Administrator
  *
  */
@@ -22,7 +26,7 @@ public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper contentMapper;
-	
+
 	/**
 	 * 查询全部
 	 */
@@ -36,8 +40,8 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public PageResult findPage(int pageNum, int pageSize) {
-		PageHelper.startPage(pageNum, pageSize);		
-		Page<TbContent> page=   (Page<TbContent>) contentMapper.selectByExample(null);
+		PageHelper.startPage(pageNum, pageSize);
+		Page<TbContent> page = (Page<TbContent>) contentMapper.selectByExample(null);
 		return new PageResult(page.getTotal(), page.getResult());
 	}
 
@@ -46,25 +50,37 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public void add(TbContent content) {
-		contentMapper.insert(content);		
+		contentMapper.insert(content);
+		// 改变了数据  需要清除缓存
+		redisTemplate.boundHashOps("content").delete(content.getCategoryId());
 	}
 
-	
 	/**
 	 * 修改
 	 */
 	@Override
-	public void update(TbContent content){
+	public void update(TbContent content) {
+		// 查询修改之前的的分类
+		Long categoryId = contentMapper.selectByPrimaryKey(content.getId()).getCategoryId();
+		redisTemplate.boundHashOps("content").delete(categoryId);
+		
 		contentMapper.updateByPrimaryKey(content);
-	}	
-	
+		
+		// 如果分类ID发生了修改,清除修改后的分类ID的缓存
+		if(categoryId.longValue() != content.getCategoryId().longValue()) {  // 注意要将对象转换为值
+			redisTemplate.boundHashOps("content").delete(content.getCategoryId());
+		}
+		
+	}
+
 	/**
 	 * 根据ID获取实体
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@Override
-	public TbContent findOne(Long id){
+	public TbContent findOne(Long id) {
 		return contentMapper.selectByPrimaryKey(id);
 	}
 
@@ -73,52 +89,70 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public void delete(Long[] ids) {
-		for(Long id:ids){
+		for (Long id : ids) {
+			
+			// 清除缓存
+			Long categoryId = contentMapper.selectByPrimaryKey(id).getCategoryId();   // 查询广告分类ID
+			redisTemplate.boundHashOps("content").delete(categoryId);
+			
 			contentMapper.deleteByPrimaryKey(id);
-		}		
+		}
 	}
-	
-	
-		@Override
+
+	@Override
 	public PageResult findPage(TbContent content, int pageNum, int pageSize) {
 		PageHelper.startPage(pageNum, pageSize);
-		
-		TbContentExample example=new TbContentExample();
+
+		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
-		
-		if(content!=null){			
-						if(content.getTitle()!=null && content.getTitle().length()>0){
-				criteria.andTitleLike("%"+content.getTitle()+"%");
+
+		if (content != null) {
+			if (content.getTitle() != null && content.getTitle().length() > 0) {
+				criteria.andTitleLike("%" + content.getTitle() + "%");
 			}
-			if(content.getUrl()!=null && content.getUrl().length()>0){
-				criteria.andUrlLike("%"+content.getUrl()+"%");
+			if (content.getUrl() != null && content.getUrl().length() > 0) {
+				criteria.andUrlLike("%" + content.getUrl() + "%");
 			}
-			if(content.getPic()!=null && content.getPic().length()>0){
-				criteria.andPicLike("%"+content.getPic()+"%");
+			if (content.getPic() != null && content.getPic().length() > 0) {
+				criteria.andPicLike("%" + content.getPic() + "%");
 			}
-			if(content.getStatus()!=null && content.getStatus().length()>0){
-				criteria.andStatusLike("%"+content.getStatus()+"%");
+			if (content.getStatus() != null && content.getStatus().length() > 0) {
+				criteria.andStatusLike("%" + content.getStatus() + "%");
 			}
-	
+
 		}
-		
-		Page<TbContent> page= (Page<TbContent>)contentMapper.selectByExample(example);		
+
+		Page<TbContent> page = (Page<TbContent>) contentMapper.selectByExample(example);
 		return new PageResult(page.getTotal(), page.getResult());
 	}
 
+	@Autowired
+	private RedisTemplate redisTemplate;  // 操作redis缓存
+
+	/**
+	 * 根据广告类型ID查询列表
+	 */
+	@Override public List<TbContent> findByCategoryId(Long categoryId) {
 		
+		// 先从缓存中获取
+		List<TbContent> contentList = (List<TbContent>) redisTemplate.boundHashOps("content").get(categoryId);
 		
-		/**
-		 * 根据广告类型ID查询列表
-		 */
-		@Override
-		public List<TbContent> findByCategoryId(Long categoryId) {
+		if(contentList == null) {
+			System.out.println("从数据库读取数据放入缓存");
 			TbContentExample example = new TbContentExample();
 			Criteria criteria = example.createCriteria();
 			criteria.andCategoryIdEqualTo(categoryId);
-			criteria.andStatusEqualTo("1");  // 开启状态
-			example.setOrderByClause("sort_order");  // 排序
-			return contentMapper.selectByExample(example);
+			criteria.andStatusEqualTo("1"); // 开启状态
+			example.setOrderByClause("sort_order"); // 排序
+			contentList = contentMapper.selectByExample(example);
+			
+			// 将从数据库中查到的数据存入缓存中
+			redisTemplate.boundHashOps("content").put(categoryId, contentList);
+			
+		} else {
+			System.out.println("从缓存中读取数据");
 		}
-	
+		return contentList;
+	}
+
 }
